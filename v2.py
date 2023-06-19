3e4172c1a69473c2bf26f68307a17810e1351505
rkaunismaa <rkaunis@gmail.com>
2023-06-19 19:39:47 -0400

    code class Head

--- v2.py
+++ v2.py
@@ -61,6 +61,35 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+                             
+    def forward(self, x):
+        B, T, C = x.shape
+        q = self.query(x) # (B, T, C)
+        k = self.key(x)   # (B, T, C)
+        v = self.value(x) # (B, T, C)
+        # compute attention scores ('affinities')
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        # perform the weighted aggregation of the values
+        out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+        
+        
+    
+
+
+
+
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
